09d34ec56126115117c692d27eea384ba3fda13f
Tibor Martini <tibor@users.noreply.github.com>
2018-05-21 22:13:28 +0200

    Update bookmarklets.php

--- bookmarklets.php
+++ bookmarklets.php
@@ -1,26 +1,26 @@
 <?php
 $bookmarklets=[
 	"Facebook Debugger"=>[
-		"bookmarklet"=>"javascript:(function()%7Blet%20d%3Ddocument%3Blet%20l%3Dd.location%3Blet%20q%3Dd.querySelector(%22link%20%5Brel%3Dcanonical%5D%22)%3B!q%20%26%26%20(q%3Dl.origin%20%2B%20l.pathname)%3Bwindow.open(%22https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Fsharing%2F%3Fq%3D%22%2BencodeURI(q))%7D)()",
+		"bookmarklet" =>"javascript:(function()%7Blet%20d%3Ddocument%3Blet%20l%3Dd.location%3B%2F%2Fget%20canonical%20urllet%20q%3Dd.querySelector(%5C%22link%20%5Brel%3Dcanonical%5D%5C%22).href%3B%2F%2Fget%20current%20tabs%20url%2C%20if%20canonical%20isn%E2%80%98t%20defined!q%20%26%26%20(q%3Dl.origin%20%2B%20l.pathname)%3B%2F%2Fopen%20facebook%20debugger%20with%20current%20url%20prefilled%20in%20new%20tabwindow.open(%5C%22https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Fsharing%2F%3Fq%3D%5C%22%2BencodeURI(q))%7D)()",
 		"title"=>"Facebook Debugger",
 		"desc"=>"Öffnet den aktuellen Link im Facebook Debugger",
 		"code"=>"let d=document;
 let l=d.location;
 //get canonical url
-let q=d.querySelector(\"link [rel=canonical]\");
+let q=d.querySelector(\"link [rel=canonical]\").href;
 //get current tabs url, if canonical isn‘t defined
 !q && (q=l.origin + l.pathname);
 //open facebook debugger with current url prefilled in new tab
 window.open(\"https://developers.facebook.com/tools/debug/sharing/?q=\"+encodeURI(q))"
 	],
 	"Link-Tweets"=>[
-		"bookmarklet"=>"javascript:(function()%7Blet%20d%3Ddocument%3Blet%20l%3Dd.location%3Blet%20q%3Dd.querySelector(%22link%20%5Brel%3Dcanonical%5D%22)%3B!q%20%26%26%20(q%3Dl.origin%20%2B%20l.pathname)%3Bwindow.open(%22https%3A%2F%2Ftwitter.com%2Fsearch%3Ff%3Dtweets%26vertical%3Ddefault%26q%3D%22%2BencodeURI(q))%7D)()",
+		"bookmarklet"=>"javascript:(function()%7Blet%20d%3Ddocument%3Blet%20l%3Dd.location%3B%2F%2Fget%20canonical%20URLlet%20q%3Dd.querySelector(%5C%22link%20%5Brel%3Dcanonical%5D%5C%22).href%3B%2F%2Fget%20current%20tabs%20URL%2C%20if%20canonical%20URL%20isn%E2%80%98t%20defined!q%20%26%26%20(q%3Dl.origin%20%2B%20l.pathname)%3B%2F%2Fopen%20twitter%20search%20with%20current%20url%20in%20new%20tabwindow.open(%5C%22https%3A%2F%2Ftwitter.com%2Fsearch%3Ff%3Dtweets%26vertical%3Ddefault%26q%3D%5C%22%2BencodeURI(q))%7D)()",
 		"title"=>"Tweets zum Link",
 		"desc"=>"Öffnet die Twitter-Suche mit Tweets zum Link",
 		"code"=>"let d=document;
 let l=d.location;
 //get canonical URL
-let q=d.querySelector(\"link [rel=canonical]\");
+let q=d.querySelector(\"link [rel=canonical]\").href;
 //get current tabs URL, if canonical URL isn‘t defined
 !q && (q=l.origin + l.pathname);
 //open twitter search with current url in new tab
@@ -28,14 +28,14 @@ window.open(\"https://twitter.com/search?f=tweets&vertical=default&q=\"+encodeUR
 "
 	],
 	"AMP-Cache"=>[
-		"bookmarklet"=>"javascript:(function()%7Blet%20d%3Ddocument%3Blet%20t%3Dd.createElement(%22a%22)%3Bt.href%3Dd.querySelector(%22*%5Brel%3D'amphtml'%5D%22).getAttribute(%22href%22)%3Blet%20e%3D%22https%3A%2F%2F%22%2Bt.host.split(%22.%22).join(%22-%22)%2B%22.cdn.ampproject.org%2Fc%2Fs%2F%22%2Bt.host%2Bt.pathname%3Bwindow.open(e)%7D)()",
+		"bookmarklet"=>"javascript:(function()%7Blet%20d%3Ddocument%3Blet%20t%3Dd.createElement(%22a%22)%3Bt.href%3Dd.querySelector(%22*%5Brel%3D'amphtml'%5D%22).href%3Blet%20e%3D%22https%3A%2F%2F%22%2Bt.host.split(%22.%22).join(%22-%22)%2B%22.cdn.ampproject.org%2Fc%2Fs%2F%22%2Bt.host%2Bt.pathname%3Bwindow.open(e)%7D)()",
 		"title"=>"AMP-Cache leeren",
 		"desc"=>"Liest den aktuellen Link in Googles AMP-Cache neu ein",
 		"code"=>"let d=document;
 //create helper to get URL
 let t=d.createElement(\"a\");
 //get URL from helper element
-t.href=d.querySelector(\"*[rel='amphtml']\").getAttribute(\"href\");
+t.href=d.querySelector(\"*[rel='amphtml']\").href;
 //build AMP cache URL from regular URL
 let e=\"https://\"+t.host.split(\".\").join(\"-\")+\".cdn.ampproject.org/c/s/\"+t.host+t.pathname;
 //open AMP cache in new tab and thus empty cache
